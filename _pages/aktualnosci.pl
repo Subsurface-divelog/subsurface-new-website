de814816ee17cb98241f901b56050550e3670074
hohndel <dirk@hohndel.org>
2019-01-02 14:57:41 -0800

    Syncing _pages/aktualnosci.pl from WordPress at https://subsurface-divelog.org (Subsurface) - wpghs

---
+++ _pages/aktualnosci.pl
@@ -0,0 +1,13 @@
+---
+ID: 82
+post_title: Aktualności
+author: hohndel
+post_date: 2019-01-02 22:57:40
+post_excerpt: ""
+layout: page
+permalink: >
+  https://subsurface-divelog.org/aktualnosci/
+published: true
+category: 'null'
+post_language: null
+---
