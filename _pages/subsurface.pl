1c3749cae565628564fb26e07e67a4fd652b32d7
hohndel <dirk@hohndel.org>
2019-01-02 14:02:52 -0800

    Syncing _pages/subsurface.pl from WordPress at https://subsurface-divelog.org (Subsurface) - wpghs

---
+++ _pages/subsurface.pl
@@ -0,0 +1,13 @@
+---
+ID: 56
+post_title: Subsurface
+author: hohndel
+post_date: 2019-01-02 22:02:52
+post_excerpt: ""
+layout: page
+permalink: https://subsurface-divelog.org/pl/
+published: true
+category: 'null'
+post_language: pl
+---
+replace with translated text
